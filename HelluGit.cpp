2256fc79eb15e84575c73466d448d4d05a8d9724
Björn <bjornax@telia.com>
2014-04-15 08:55:39 +0200

    Crazy hip text
    Added crazy hip and cool text concerning the nature of this
    project...pretty cool...

--- HelluGit.cpp
+++ HelluGit.cpp
@@ -2,10 +2,14 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cout << "Hellu you crazy Git!" << endl;
+	cin.get();
 	return 0;
 }
 
